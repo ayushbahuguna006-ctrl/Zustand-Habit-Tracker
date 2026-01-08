6acf5988157ef1a2f3eedc49e60a08d8cca732df
Ayush Bahuguna <Ayushbahuguna006@gmail.com>
2026-01-08 11:29:12 +0530

    Project initialized

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <p className='text-red-500 text-center'>HELLO</p>
+    </>
+  )
+}
+
+export default App
